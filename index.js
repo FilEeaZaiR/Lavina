c7d3119e879e7868bb4fd362132e10943a328906
FilEeaZaiR <42099557+FilEeaZaiR@users.noreply.github.com>
2019-06-07 20:45:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ client.on(`message`, message =>{
                 fields: [
                 {
                         name: "***Règles :***",
-                    value: " • Ne pas insulter \n • Ne pas spam \n • Ne pas abuser des majuscules \n • Respecter les autres membres du Discord ainsi que le Staff \n • Avoir une bonne écriture/orthographe \n • Ne pas pub sauf dans le salon conçu spécialement pour \n • Ne pas faire les commandes bots dans le général ou autres mais bien dans le salon prévu à cet effet",
+                    value: " • Ne pas insulter \n • Ne pas spam \n • Ne pas abuser des majuscules \n • Respecter les autres membres du Discord ainsi que le Staff \n • Ne pas mentionner les membres du staff inutilement \n • Avoir une bonne écriture/orthographe \n • Ne pas pub sauf dans le salon conçu spécialement pour \n • Ne pas faire les commandes bots dans le général ou autres mais bien dans le salon prévu à cet effet",
                     inline: true
                 },
                 {
